61c806506073340e3936b44c045c669dee809f32
Michaella Rodriguez <micha.ella@hotmail.com>
2024-09-01 10:08:19 -0500

    button and link styling for different states

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,7 +9,7 @@ export default function Greeting({messages}) {
   return (
     <div>
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
+      <button class="greeting-btn" onClick={() => setGreeting(randomMessage())}>
         New Greeting
       </button>
     </div>
